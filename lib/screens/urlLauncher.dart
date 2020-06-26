e8113f26fa3d62fc748debe247da708c80ffbe3c
DESKTOP-RHMNSMK\KnitidCeladon23 <kenny.chewpuyang23@gmail.com>
2020-06-27 00:16:24 +0800

    adjust buttons to center

--- lib/screens/urlLauncher.dart
+++ lib/screens/urlLauncher.dart
@@ -116,52 +116,53 @@ class _URLLauncherState extends State<URLLauncher> {
           ),
         ),
       ),
-      body: Container(
-        color: Colors.brown,
-        child: ListView(
-          children: <Widget>[
-            const Padding(padding: EdgeInsets.all(16.0)),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Material(
-                  elevation: 5.0,
-                  child: RaisedButton(
-                    color: Colors.red[200],
-                    padding: EdgeInsets.fromLTRB(20, 20.0, 20, 20.0),
-                    onPressed: () => setState(() {
-                      _launched = _launchInBrowser(toLaunch);
-                    }),
-                    child: Text("Launch in Browser",
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.kalam(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        )),
+      body: Center(
+        child: Container(
+          color: Colors.brown,
+          child: ListView(
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Material(
+                    elevation: 5.0,
+                    child: RaisedButton(
+                      color: Colors.red[200],
+                      padding: EdgeInsets.fromLTRB(20, 20.0, 20, 20.0),
+                      onPressed: () => setState(() {
+                        _launched = _launchInBrowser(toLaunch);
+                      }),
+                      child: Text("Launch in Browser",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.kalam(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          )),
+                    ),
                   ),
-                ),
-                const Padding(padding: EdgeInsets.all(16.0)),
-                Material(
-                  elevation: 5.0,
-                  child: RaisedButton(
-                    color: Colors.red[200],
-                    padding: EdgeInsets.fromLTRB(20, 20.0, 20, 20.0),
-                    onPressed: () => setState(() {
-                      _launched = _launchInWebViewOrVC(toLaunch);
-                    }),
-                    child: Text("Launch in App",
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.kalam(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        )),
+                  const Padding(padding: EdgeInsets.all(16.0)),
+                  Material(
+                    elevation: 5.0,
+                    child: RaisedButton(
+                      color: Colors.red[200],
+                      padding: EdgeInsets.fromLTRB(20, 20.0, 20, 20.0),
+                      onPressed: () => setState(() {
+                        _launched = _launchInWebViewOrVC(toLaunch);
+                      }),
+                      child: Text("Launch in App",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.kalam(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          )),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
