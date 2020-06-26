4ab21100d3c3e6e68c08792fb888b94b4eeb1b65
DESKTOP-RHMNSMK\KnitidCeladon23 <kenny.chewpuyang23@gmail.com>
2020-06-27 00:29:51 +0800

    remove padding widget

--- lib/screens/urlLauncher.dart
+++ lib/screens/urlLauncher.dart
@@ -119,49 +119,46 @@ class _URLLauncherState extends State<URLLauncher> {
       body: Center(
         child: Container(
           decoration: BoxDecoration(color: Colors.brown),
-          child: Padding(
-            padding: const EdgeInsets.all(50.4),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Material(
-                  elevation: 5.0,
-                  child: RaisedButton(
-                    color: Colors.red[200],
-                    padding: EdgeInsets.fromLTRB(20, 20.0, 20, 20.0),
-                    onPressed: () => setState(() {
-                      _launched = _launchInBrowser(toLaunch);
-                    }),
-                    child: Text("Launch in Browser",
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.kalam(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        )),
-                  ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Material(
+                elevation: 5.0,
+                child: RaisedButton(
+                  color: Colors.red[200],
+                  padding: EdgeInsets.fromLTRB(20, 20.0, 20, 20.0),
+                  onPressed: () => setState(() {
+                    _launched = _launchInBrowser(toLaunch);
+                  }),
+                  child: Text("Launch in Browser",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.kalam(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      )),
                 ),
-                const Padding(padding: EdgeInsets.all(16.0)),
-                Material(
-                  elevation: 5.0,
-                  child: RaisedButton(
-                    color: Colors.red[200],
-                    padding: EdgeInsets.fromLTRB(20, 20.0, 20, 20.0),
-                    onPressed: () => setState(() {
-                      _launched = _launchInWebViewOrVC(toLaunch);
-                    }),
-                    child: Text("Launch in App",
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.kalam(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        )),
-                  ),
+              ),
+              const Padding(padding: EdgeInsets.all(16.0)),
+              Material(
+                elevation: 5.0,
+                child: RaisedButton(
+                  color: Colors.red[200],
+                  padding: EdgeInsets.fromLTRB(20, 20.0, 20, 20.0),
+                  onPressed: () => setState(() {
+                    _launched = _launchInWebViewOrVC(toLaunch);
+                  }),
+                  child: Text("Launch in App",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.kalam(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      )),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
