41f7de8ca9863bd9174ca74050909fb9d5f41ce6
DESKTOP-RHMNSMK\KnitidCeladon23 <kenny.chewpuyang23@gmail.com>
2020-06-27 18:56:30 +0800

    added remind me and recipe buttons to calendar item viewer

--- lib/calendar/ui/pages/view_event.dart
+++ lib/calendar/ui/pages/view_event.dart
@@ -1,3 +1,5 @@
+import 'package:expiree_app/notification/createNotifPage.dart';
+import 'package:expiree_app/screens/urlLauncher.dart';
 import 'package:flutter/material.dart';
 import '../../model/event.dart';
 
@@ -8,6 +10,22 @@ class EventDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void moveToURL(String foodItem) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => URLLauncher(
+                  title: "Recipe for " + foodItem, foodItem: foodItem)));
+    }
+
+    Future<void> navigateToNotificationCreation() async {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => CreateNotificationPage(),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Event details'),
@@ -21,8 +39,34 @@ class EventDetailsPage extends StatelessWidget {
               event.item,
               style: Theme.of(context).textTheme.headline4,
             ),
-             SizedBox(height: 20.0),
-             Text(event.description)
+            SizedBox(height: 20.0),
+            Text(event.description),
+            SizedBox(height: 10.0),
+            RaisedButton(
+              color: Colors.brown[400],
+              padding: EdgeInsets.all(8.0),
+              onPressed: () => moveToURL(event.item),
+              child: Text(
+                'Recipes',
+                style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            RaisedButton(
+              color: Colors.grey[700],
+              padding: EdgeInsets.all(8.0),
+              onPressed: navigateToNotificationCreation,
+              child: Text(
+                'Remind me!',
+                style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
           ],
         ),
       ),
