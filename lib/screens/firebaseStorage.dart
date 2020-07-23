b4fb54764752d21740911d0e7b14c6c917e9c868
DESKTOP-RHMNSMK\KnitidCeladon23 <kenny.chewpuyang23@gmail.com>
2020-07-23 21:49:57 +0800

    redo merge to avoid any wrong replacements for conflicts, the only change would be the addition of two new dart files

---
+++ lib/screens/firebaseStorage.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/widgets.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+class FirebaseStoragePage extends StatefulWidget {
+  final File file;
+  final String userID;
+  final String itemID;
+  FirebaseStoragePage({Key key, this.file, this.userID, this.itemID})
+      : super(key: key);
+  @override
+  _FirebaseStoragePageState createState() => _FirebaseStoragePageState();
+}
+
+class _FirebaseStoragePageState extends State<FirebaseStoragePage> {
+  final FirebaseStorage _storage =
+      FirebaseStorage(storageBucket: 'gs://expiree-login-51433.appspot.com/');
+
+  StorageUploadTask _uploadTask;
+
+  /// Starts an upload task
+  void _startUpload() {
+    /// Unique file name for the file
+    String filePath = 'images/${widget.userID}/${widget.itemID}.png';
+
+    setState(() {
+      _uploadTask = _storage.ref().child(filePath).putFile(widget.file);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_uploadTask != null) {
+      /// Manage the task state and event subscription with a StreamBuilder
+      return StreamBuilder<StorageTaskEvent>(
+          stream: _uploadTask.events,
+          builder: (_, snapshot) {
+            var event = snapshot?.data?.snapshot;
+
+            double progressPercent = event != null
+                ? event.bytesTransferred / event.totalByteCount
+                : 0;
+
+            return Column(
+              children: [
+                if (_uploadTask.isComplete)
+                  RaisedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("Return")),
+
+                if (_uploadTask.isPaused)
+                  FlatButton(
+                    child: Icon(Icons.play_arrow),
+                    onPressed: _uploadTask.resume,
+                  ),
+
+                if (_uploadTask.isInProgress)
+                  FlatButton(
+                    child: Icon(Icons.pause),
+                    onPressed: _uploadTask.pause,
+                  ),
+
+                // Progress bar
+                LinearProgressIndicator(value: progressPercent),
+                Text('${(progressPercent * 100).toStringAsFixed(2)} % '),
+              ],
+            );
+          });
+    } else {
+      // Allows user to decide when to start the upload
+      return FlatButton.icon(
+        label: Text('Upload to Firebase'),
+        icon: Icon(Icons.cloud_upload),
+        onPressed: _startUpload,
+      );
+    }
+  }
+}
